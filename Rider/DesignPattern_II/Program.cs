3392891b20b22163d70546ee8f3e9d7b5056681c
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-07-26 10:52:25 +0200

    dev

--- Rider/DesignPattern_II/Program.cs
+++ Rider/DesignPattern_II/Program.cs
@@ -1,13 +1,14 @@
-﻿using static System.Console;
+﻿using DesignPattern_II;
+using static System.Console;
 
 namespace DesignPattern_II
 {
 
-    public class Rectangle          // Represents a rectangle with width and height.
+    public class Rectangle                                                 // Represents a rectangle with width and height.
     {
-        public int Width { get; }   // Gets the width of the rectangle.
+        public int Width { get; set; } // Gets the width of the rectangle.
 
-        public int Height { get; }      // Gets the height of the rectangle.
+        public int Height { get; set; } // Gets the height of the rectangle.
 
         public Rectangle(int width, int height)         // Initializes a new instance of the Rectangle class with the specified width and height.
         {
@@ -22,6 +23,28 @@ namespace DesignPattern_II
         }
     }
 
+    public class Square : Rectangle
+    {
+        private int side;
+
+        public Square(int side) : base(side, side)
+        {
+            this.side = side;
+        }
+
+        public new int Width
+        {
+            get { return base.Width; }
+            set { base.Width = base.Height = value; }
+        }
+
+        public new int Height
+        {
+            get { return base.Height; }
+            set { base.Width = base.Height = value; }
+        }
+    }
+    }
     public class Demo
     {
 
@@ -31,6 +54,9 @@ namespace DesignPattern_II
         {
             var rcRectangle = new Rectangle(2, 3);
             WriteLine($"{rcRectangle} has area {Area(rcRectangle)}");
+
+            var sqSquare = new Square();
+            sqSquare.Width = 4;
+            WriteLine($"{sqSquare} has area {Area(rcRectangle)}");
         }
     }
-}
